419fff5d596f8f8401888dfd0cdc92ed9858e1d7
ItsMagick <magick930@googlemail.com>
2025-04-23 17:25:07 +0200

    - added some scripts for installing deps and running ansible

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-ansible-playbook -i inventory/hosts playbooks/main.yml --ask-become-pass --verbose 
+#!/bin/bash
+set -ex
+
+# Install core dependencies
+echo "Installing core dependencies in helpers/install_core_dependencies.sh \n"
+./helpers/install_core_dependencies.sh
+
+# Let the fun begin... installing your setup
+echo "Installing your setup in helpers/run_ansible.sh \n"
+./helpers/run_ansible.sh
+
